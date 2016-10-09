49ccf5480407618c86cbf44b9666ee27b1f9a6dd
Poena1 <jordanwilliams977@hotmail.com>
2016-10-10 00:58:13 +0100

    testing.

---
+++ TestCompany.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestCompany;
+
+/**
+ *
+ * @author x00130180
+ */
+public class TestCompany {
+    public static void main(String[] args) {
+        Staff s = new Staff();
+        s.processStaff();
+        s.payday();
+    }
+}
